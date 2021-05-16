979c1692ac86ce64bd149c34250c76984725df8d
Evgenii Sleptsov <gladluter@gmail.com>
2021-05-16 23:02:50 +0300

    Farmer game

---
+++ LabWork131/Hello_Operators/Programs/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOperators
+{
+    class Calculator
+    {
+        public void Run()
+        {
+            // Set Console.ForegroundColor  value
+            Console.WriteLine(@"Select the arithmetic operation:
+1. Multiplication 
+2. Divide 
+3. Addition 
+4. Subtraction
+5. Exponentiation ");
+            // Implement option input (1,2,3,4,5)
+            //     and input of  two or one numbers
+            //  Perform calculations and output the result 
+
+        }
+    }
+}
---
+++ LabWork131/Hello_Operators/Programs/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOperators
+{
+    class Factorial
+    {
+        public void Run()
+        {
+           //Implement input of the number
+            // Implement input the for circle to calculate factorial of the number
+            // Output the result
+
+        }
+    }
+}
