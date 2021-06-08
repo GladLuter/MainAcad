9b4d1e351923b7b7f3fee254be14bf6848b8f367
Evgenii Sleptsov <gladluter@gmail.com>
2021-06-09 00:26:14 +0300

    add default data

---
+++ LabWork122/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LabaWork1221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Implement start position, width and height, symbol, message input
+
+                //Create Box class instance
+
+                //Use  Box.Draw() method
+
+                Console.WriteLine("Press any key...");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+
+    }
+}
