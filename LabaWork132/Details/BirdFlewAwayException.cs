7e14e025b220fdc8f1c42ab8a602a90849876565
Evgenii Sleptsov <gladluter@gmail.com>
2021-06-13 15:15:29 +0300

    add default for 132

---
+++ LabaWork132/Details/BirdFlewAwayException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaWork132.Details
+{
+    //Create the BirdFlewAwayException class, derived from ApplicationException  with two properties  
+
+    //When
+    //Why
+
+    //Create constructors
+}
