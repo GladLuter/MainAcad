cfea11d912809ff08d63e58dbbfe1beeb8a76e9f
Evgenii Sleptsov <gladluter@gmail.com>
2021-06-16 23:10:17 +0300

    Add default 141

---
+++ LabaWork141/Details/GoodInfoEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaWork141
+{
+    // 1) inherit EventArgs
+    class GoodsInfoEventArgs : EventArgs
+    {
+        // 2) declare property GoodsName
+        // think about get and set attributes
+
+
+        // 3) declare constructor to initialize GoodsName
+
+    }
+}
