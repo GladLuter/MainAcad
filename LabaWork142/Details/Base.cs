05fcec91845d365fc16903d9c397be79d3a0c4b2
Evgenii Sleptsov <gladluter@gmail.com>
2021-06-17 23:45:42 +0300

    142

---
+++ LabaWork142/Details/Base.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaWork142
+{
+    public class Base<T> where T : new()
+    {
+        static Base()
+        {
+            System.Console.WriteLine("  Base static constructor");
+            T intern = new T();
+        }
+
+    }
+}
---
+++ LabaWork142/Details/Derived_static_field.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaWork142
+{
+    public sealed class Derived_static_field : Base_static_field<Derived_static_field>
+    {
+        public Derived_static_field()
+        {
+            System.Console.WriteLine("      Derived constructor");
+        }
+    }
+}
